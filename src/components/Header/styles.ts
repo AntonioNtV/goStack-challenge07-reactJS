f7a91d2538ba14ceb43e830a37c0bd4a1afcb039
AntonioNtV <antoniontv18@gmail.com>
2020-09-02 22:19:33 -0300

    Estilizaçao do botao de navegaçao. Adicionando um bordar laranja na parte de baixo do botao

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -21,14 +21,13 @@ export const Container = styled.div<ContainerProps>`
         color: #fff;
         text-decoration: none;
         font-size: 16px;
-        transition: opacity 0.2s;
 
         & + a {
           margin-left: 32px;
         }
 
         &:hover {
-          opacity: 0.6;
+          border-bottom: 2px solid #ff872c;
         }
       }
     }

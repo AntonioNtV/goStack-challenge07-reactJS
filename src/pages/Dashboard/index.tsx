5e82c3c06ec11a34a1a835d86783a629d840bf5d
AntonioNtV <antoniontv18@gmail.com>
2020-09-02 22:10:35 -0300

    Listagem de transaçoes

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -56,7 +56,8 @@ const Dashboard: React.FC = () => {
         outcome: formatValue(balance.outcome),
         total: formatValue(balance.total),
       };
-      setTransactions(transactions);
+
+      setTransactions(serializedTransactions);
       setBalance(serializedBalance);
     }
 
@@ -101,21 +102,19 @@ const Dashboard: React.FC = () => {
                 <th>Data</th>
               </tr>
             </thead>
-
-            <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
-            </tbody>
+            {transactions.map(transaction => (
+              <tbody key={transaction.id}>
+                <tr>
+                  <td className="title">{transaction.title}</td>
+                  <td className={transaction.type}>
+                    {transaction.type === 'outcome' && '- '}
+                    {transaction.formattedValue}
+                  </td>
+                  <td>{transaction.category.title}</td>
+                  <td>{transaction.formattedDate}</td>
+                </tr>
+              </tbody>
+            ))}
           </table>
         </TableContainer>
       </Container>
